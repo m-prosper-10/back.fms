3adb79210461a369c51430a350b1b89a02a2167c
Polo <nelsonprox92@gmail.com>
2026-06-03 08:23:45 +0000

    Implement CRUD operations and authorization for fire extinguisher management module

---
+++ services/extinguisher-service/src/config/database.ts
@@ -0,0 +1,61 @@
+import { Collection, Db, MongoClient, ObjectId } from "mongodb";
+import { AppError } from "../../../shared/lib/httpError";
+import { logger } from "../../../shared/lib/logger";
+import { env } from "./env";
+import { ExtinguisherDocument } from "../modules/extinguishers/extinguisher.types";
+
+let client: MongoClient | null = null;
+let database: Db | null = null;
+
+export interface ExtinguisherCollections {
+  extinguishers: Collection<ExtinguisherDocument>;
+}
+
+export async function connectExtinguisherDatabase() {
+  if (database) {
+    return database;
+  }
+
+  client = new MongoClient(env.raw.MONGODB_URL);
+  await client.connect();
+  database = client.db();
+
+  await Promise.all([
+    database
+      .collection<ExtinguisherDocument>("fire_extinguishers")
+      .createIndex({ serialNumber: 1 }, { unique: true }),
+    database.collection<ExtinguisherDocument>("fire_extinguishers").createIndex({ status: 1 }),
+    database.collection<ExtinguisherDocument>("fire_extinguishers").createIndex({ location: 1 }),
+    database.collection<ExtinguisherDocument>("fire_extinguishers").createIndex({ expiryDate: 1 }),
+    database.collection<ExtinguisherDocument>("fire_extinguishers").createIndex({ type: 1 })
+  ]);
+
+  logger.info(`Connected to MongoDB database ${database.databaseName}`);
+  return database;
+}
+
+export function getExtinguisherDatabase() {
+  if (!database) {
+    throw new AppError(500, "Extinguisher database is not connected");
+  }
+
+  return database;
+}
+
+export function getExtinguisherCollections(): ExtinguisherCollections {
+  const db = getExtinguisherDatabase();
+
+  return {
+    extinguishers: db.collection<ExtinguisherDocument>("fire_extinguishers")
+  };
+}
+
+export async function closeExtinguisherDatabase() {
+  await client?.close();
+  client = null;
+  database = null;
+}
+
+export function toObjectId(id: string) {
+  return new ObjectId(id);
+}
